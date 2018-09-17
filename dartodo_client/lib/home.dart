02125f5f279cbbfee8566e518be2ffed1048a2fc
Sudharshan <sudhar393@gmail.com>
2018-09-17 19:50:09 +0800

    need to find time to do this ugh

--- dartodo_client/lib/home.dart
+++ dartodo_client/lib/home.dart
@@ -12,32 +12,49 @@ class Home extends StatelessWidget {
             tooltip: "Filter",
             onPressed: null,
           ),
+          IconButton(
+            icon: Icon(Icons.favorite),
+            tooltip: "Favorites",
+            onPressed: null,
+          )
         ],
+      ),
+      body: ListItem(
+        name: 'Sudharshan',
+        favorite: false,
+        tags: []
       ),
     );
   }
 }
 
- class TodoListView extends StatefulWidget {
+ class ListItem extends StatelessWidget {
+   final String name;
+   final bool favorite;
+   final List<String> tags;
+   final Widget child;
+
+   ListItem({
+     Key key,
+     this.name,
+     this.favorite: false,
+     this.tags,
+     this.child,
+   }) : super(key: key);
+
    @override
-   _TodoListViewState createState() => _TodoListViewState();
- }
- 
- class _TodoListViewState extends State<TodoListView> {
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(
-         title: Text('text'),
-       ),
-       body: Container(
-         padding: EdgeInsets.all(32.0),
-         child: Center(
-           child: Column(
-             children: <Widget>[],
-           ),
-         ),
-       ),
-     );
+   Widget build(BuildContext context){
+     return ListTile(
+      leading: Icon(Icons.favorite_border),
+      title: Text(name),
+    );
    }
+ }
+
+ class ListData {
+   final String name;
+   final bool favorite;
+   final List<String> tags;
+
+   ListData(this.name, this.favorite, this.tags);
  }
