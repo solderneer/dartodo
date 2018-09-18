dcd40454b489d692f5f7df5d1e721f420b4608cd
Sudharshan <sudhar393@gmail.com>
2018-09-18 18:17:51 +0800

    idk anymore

--- dartodo_client/lib/home.dart
+++ dartodo_client/lib/home.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:badge/badge.dart';
 
 class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Dartodo"),
@@ -19,42 +20,61 @@ class Home extends StatelessWidget {
           )
         ],
       ),
-      body: ListItem(
-        name: 'Sudharshan',
-        favorite: false,
-        tags: []
+      body: Column(
+        children: <Widget>[
+          ListItem(
+            name: "Do assignment 3",
+            favorite: false,
+            tags: ["Work", "Stuff"],
+          ),
+          ListItem(
+            name: "Do assignment 3",
+            favorite: false,
+            tags: ["Work", "Stuff"],
+          ),
+        ],
       ),
     );
   }
 }
 
- class ListItem extends StatelessWidget {
-   final String name;
-   final bool favorite;
-   final List<String> tags;
-   final Widget child;
+class ListItem extends StatelessWidget {
+  final String name;
+  final bool favorite;
+  final List<String> tags;
+  final Widget child;
 
-   ListItem({
-     Key key,
-     this.name,
-     this.favorite: false,
-     this.tags,
-     this.child,
-   }) : super(key: key);
+  ListItem({
+    Key key,
+    this.name,
+    this.favorite: false,
+    this.tags,
+    this.child,
+  }) : super(key: key);
 
-   @override
-   Widget build(BuildContext context){
-     return ListTile(
-      leading: Icon(Icons.favorite_border),
-      title: Text(name),
-    );
-   }
- }
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: new Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.favorite_border),
+          title: Text(this.name),
+        ),
+        Badge(
+          value: "Work",
+          child: Text(""),
+        ),
+      ],
+    ));
+  }
+}
 
- class ListData {
-   final String name;
-   final bool favorite;
-   final List<String> tags;
+class ListData {
+  final String name;
+  final bool favorite;
+  final List<String> tags;
 
-   ListData(this.name, this.favorite, this.tags);
- }
+  ListData(this.name, this.favorite, this.tags);
+}
